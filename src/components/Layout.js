3a73b18c0e9b8462fc04c7065ae0e28bd990e081
Stackbit <projects@stackbit.com>
2020-12-20 17:25:21 +0000

    initial commit by Stackbit

---
+++ src/components/Layout.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Head from 'next/head';
+import _ from 'lodash';
+
+import Header from './Header';
+import Footer from './Footer';
+
+export default class Body extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Head>
+                    <title>{_.get(this.props, 'page.frontmatter.title', null) && (_.get(this.props, 'page.frontmatter.title', null) + ' - ')}{_.get(this.props, 'data.config.title', null)}</title>
+                    <meta charSet="utf-8"/>
+                    <meta name="viewport" content="width=device-width, initialScale=1.0" />
+                    <meta name="description" content={_.get(this.props, 'page.frontmatter.excerpt', null) || _.get(this.props, 'data.config.description', null)}/>
+                    <link href="https://fonts.googleapis.com/css?family=Lato:400,400i,700,700i%26Display=swap" rel="stylesheet"/> 
+                </Head>
+                <div id="page" className="site">
+                  <Header {...this.props} />
+                  <main id="content" className="site-content">
+                    {this.props.children}
+                  </main>
+                  <Footer {...this.props} />
+                </div>
+            </React.Fragment>
+        );
+    }
+}
---
+++ src/components/index.js
@@ -0,0 +1,45 @@
+import ActionLink from './ActionLink';
+import CtaButtons from './CtaButtons';
+import DocsMenu from './DocsMenu';
+import DocsSubmenu from './DocsSubmenu';
+import Footer from './Footer';
+import Header from './Header';
+import Layout from './Layout';
+import SectionContent from './SectionContent';
+import SectionCta from './SectionCta';
+import SectionDocs from './SectionDocs';
+import SectionGrid from './SectionGrid';
+import SectionHero from './SectionHero';
+import Submenu from './Submenu';
+
+export {
+    ActionLink,
+    CtaButtons,
+    DocsMenu,
+    DocsSubmenu,
+    Footer,
+    Header,
+    Layout,
+    SectionContent,
+    SectionCta,
+    SectionDocs,
+    SectionGrid,
+    SectionHero,
+    Submenu
+};
+
+export default {
+    ActionLink,
+    CtaButtons,
+    DocsMenu,
+    DocsSubmenu,
+    Footer,
+    Header,
+    Layout,
+    SectionContent,
+    SectionCta,
+    SectionDocs,
+    SectionGrid,
+    SectionHero,
+    Submenu
+};
